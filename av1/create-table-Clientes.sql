ff3e7a85e7e467a8a313374da64c396d2a0470bf
RafaelaAlexandre <rafaela.a.oliveira1998@gmail.com>
2024-05-06 22:15:57 -0300

    :sparkles: feat: Adicionando script de criação da tabela Clientes

---
+++ av1/create-table-Clientes.sql
@@ -0,0 +1,16 @@
+
+CREATE TABLE Clientes (
+    cpf VARCHAR(11) PRIMARY KEY,
+    buyer_email VARCHAR(255),
+    buyer_name VARCHAR(255),
+    buyer_phone_number VARCHAR(20),
+    ship_address_1 VARCHAR(255),
+    ship_address_2 VARCHAR(255),
+    ship_address_3 VARCHAR(255),
+    ship_city VARCHAR(100),
+    ship_state VARCHAR(100),
+    ship_postal_code VARCHAR(20),
+    ship_country VARCHAR(100)
+    
+);
+
